e3acd9f095fc97321981ead287aa80939ce3b874
Yongzhi Huang <yhuang@fastcompany.com>
2016-12-22 09:43:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Simple Websocket based chat in Go
+
+##Installation
+
+```
+go get github.com/gorilla/websocket
+cd src
+go run main.go
+```
---
+++ src/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{}
+
+// Message object
+type Message struct {
+    Email string `json:"email"`
+    Username string `json:"username"`
+    Message string `json:"message"`
+}
+
+func main() {
+    fs := http.FileServer(http.Dir("../public"))
+    http.Handle("/", fs)
+
+    // Handle websocket routes
+    http.HandleFunc("/ws", handleConnections)
+
+    go handleMessages()
+
+    log.Println("Http server started on :8000")
+    err := http.ListenAndServe(":8000", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+    // Upgrade initial GET request to a websocket
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    defer ws.Close()
+    
+    // Register new client
+    clients[ws] = true
+
+    for {
+        var msg Message
+
+        err := ws.ReadJSON(&msg)
+        if err != nil {
+            log.Printf("error: %v", err)
+            delete(clients, ws)
+            break
+        }
+        broadcast <- msg
+    }
+}
+
+func handleMessages() {
+    for {
+        msg := <-broadcast
+        
+        for client := range clients {
+            err := client.WriteJSON(msg)
+            if err != nil {
+                log.Printf("error: %v", err)
+                client.Close()
+                delete(clients, client)
+            }
+        }
+    }
+}
